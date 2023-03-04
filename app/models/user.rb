92411102dc8f7049c7758faf093036f1c1d9256c
kimathinjoki <dfkimathinjoki@gmail.com>
2023-03-04 10:26:18 -0800

    create user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,19 @@
 class User < ActiveRecord::Base
 
+    has_many :links
+    has_many :projects, through: :links
+
+    # table consists of password_hash as a column to store password hashes in DB
+  include BCrypt
+
     # retrieve password from hash
   def password
     @password ||= Password.new(passwordHash)
   end
 
   # create the password hash
-  def password=(pass)
-    @password = Password.create(pass)
-    self.password_hash = @password
+  def password=(pw)
+    @password = Password.create(pw)
+    self.passwordHash = @password
   end
 end
